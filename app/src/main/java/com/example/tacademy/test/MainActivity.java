45e1722443afc85d7aa9a6519b44aed86a26d838
ChaPark <kureum12@naver.com>
2016-07-11 14:41:41 +0900

    dddd

--- app/src/main/java/com/example/tacademy/test/MainActivity.java
+++ app/src/main/java/com/example/tacademy/test/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.tacademy.test;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputView;
+    TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputView = (EditText)findViewById(R.id.edit_input);
+        messageView=(TextView)findViewById(R.id.messageView);
+
+        Button btn = (Button)findViewById(R.id.btn_input);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = inputView.getText().toString();
+                messageView.setText(text);
+
+            }
+        });
+
+
+        btn = (Button)findViewById(R.id.btn_toast);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_message , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        btn = (Button)findViewById(R.id.btn_google);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com/"));
+                startActivity(intent);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_dial);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-5210-1218"));
+                startActivity(intent);
+            }
+        });
+
+        btn =(Button)findViewById(R.id.btn_my);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
